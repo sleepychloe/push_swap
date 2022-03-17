ff93b29a9647851784d719400395247c2fee939d
sleepychloe <hyj2132@gmail.com>
2022-03-17 03:29:34 +0100

    20220317_03:29

--- libft/incs/libft.h
+++ libft/incs/libft.h
@@ -51,7 +51,7 @@ void		ft_putstr_fd(char *s, int fd);
 char		**ft_split(char const *s, char c);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strdup(const char *src);
-void		ft_striteri(char *s, void(*f)(unsigned int, char *));
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 char		*ft_strjoin(char const *s1, char const *s2);
 size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
